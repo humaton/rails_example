f573fbbb83767aa07c1e35217e8c666c721262e7
thrcka <thrcka@unimatrix-01.(none)>
2012-04-03 22:37:24 +0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsExample::Application.config.secret_token = '602f65c4f34273ff59eab2f14d53e3d763c978413d588951ab76d805a054beec84b18bdb28a58d97b09d06f6ddcdedfe8decbc792c5bc1fdd801c327101a8644'
